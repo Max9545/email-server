4977e2f3e8b40d00258bc55fcb326f19135ce743
Max Bregman <bregmanmax91@gmail.com>
2024-01-05 13:29:28 -0700

    Port change

--- bin/server.js
+++ bin/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const { main } = require('.');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3005;
 const yargs = require('yargs');
 
 require('dotenv').config();
