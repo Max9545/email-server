1fd8ed164517062b06b5925c99bf1bbd0b4131a3
Max Bregman <bregmanmax91@gmail.com>
2024-01-17 12:38:03 -0700

    Modify Cors

--- bin/server.js
+++ bin/server.js
@@ -15,6 +15,13 @@ const corsOptions = {
 
 app.use(express.json());
  
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.post('/sendMail',cors(corsOptions), async (req, res) => {
   console.log('body', req.body)
   // main()
